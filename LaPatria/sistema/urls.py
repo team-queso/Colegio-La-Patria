c44aaa3fc9293016465ffdf2c60c8fb072cf0bf2
Alejandro <ldonquixote@hotmail.com>
2019-03-10 23:04:24 -0600

    Cambios

--- LaPatria/sistema/urls.py
+++ LaPatria/sistema/urls.py
@@ -8,8 +8,12 @@ urlpatterns = [
     path('primaria/informacion', views.informacion, name='informacion'),
     path('primaria/horarios', views.horarios, name='horarios'),
     path('primaria/calificaciones', views.calificaciones, name='calificaciones'),
-    path('primaria/reportes', views.calificaciones, name='reportes'),
-
+    path('primaria/reportes', views.reportes, name='reportes'),
+    path('secundaria/', views.secundaria, name='secundaria'),
+    path('secundaria/informacion', views.informacion_secundaria, name='informacion2'),
+    path('secundaria/horarios', views.horarios_secundaria, name='horarios2'),
+    path('secundaria/calificaciones', views.calificaciones_secundaria, name='calificaciones2'),
+    path('secundaria/reportes', views.reportes_secundaria, name='reportes2'),
 
 
 ]
