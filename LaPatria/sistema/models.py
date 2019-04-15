18c1fb0198d1462883deba4069f454d445c9b254
Alejandro <ldonquixote@hotmail.com>
2019-04-15 08:35:26 -0500

    Modelos actualizados

--- LaPatria/sistema/models.py
+++ LaPatria/sistema/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class admin(models.Model):
+    usuario = models.CharField(max_length=40)
+    password = models.CharField(max_length=15)
+
