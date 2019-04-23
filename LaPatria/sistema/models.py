36dc8340643a7e13a73598b1e9a63bf999978478
Alejandro <ldonquixote@hotmail.com>
2019-04-23 17:40:21 -0500

    models

--- LaPatria/sistema/models.py
+++ LaPatria/sistema/models.py
@@ -5,3 +5,11 @@ class administrador(models.Model):
     usuario = models.CharField(max_length=40)
     password = models.CharField(max_length=15)
 
+class alumno(models.Model):
+    idAlumno = models.IntegerField(max_length=45)
+    no_control = models.CharField(max_length=45)
+    nombre = models.CharField(max_length=45)
+    telefono = models.IntegerField(max_length=10)
+    domicilio = models.CharField(max_length=150)
+    grado = models.IntegerField(max_length=1)
+    ciclo = models.CharField(max_length=9)
