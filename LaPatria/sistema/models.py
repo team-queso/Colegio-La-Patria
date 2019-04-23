d8b3f7f9e2a752c7221cea632368e9b14a83df9a
Alejandro <ldonquixote@hotmail.com>
2019-04-23 18:04:44 -0500

    Prueba

--- LaPatria/sistema/models.py
+++ LaPatria/sistema/models.py
@@ -7,9 +7,9 @@ class administrador(models.Model):
 
 class alumno(models.Model):
     idAlumno = models.IntegerField(max_length=45)
-    no_control = models.CharField(max_length=45)
+    no_control = models.IntegerField(max_length=15)
     nombre = models.CharField(max_length=45)
-    telefono = models.IntegerField(max_length=10)
+    telefono = models.IntegerField()
     domicilio = models.CharField(max_length=150)
-    grado = models.IntegerField(max_length=1)
+    grado = models.IntegerField(blank="false")
     ciclo = models.CharField(max_length=9)
