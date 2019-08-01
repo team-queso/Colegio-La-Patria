981c90908fa752fcc6deff2edbd2435855bd72c6
Alejandro <ldonquixote@hotmail.com>
2019-08-01 13:58:07 -0500

    Login Funcional pero feo

--- LaPatria/LaPatria/urls.py
+++ LaPatria/LaPatria/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
 
 	path('colegiolapatria/', include('sistema.urls')),
-    path('',include('registros.urls')),
+    path('' , LoginView.as_view(), name ='login' ),
+    path('logout/',LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
    
 
--- LaPatria/registros/views.py
+++ LaPatria/registros/views.py
@@ -4,22 +4,4 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
 
 # Create your views here.
-def index(request):
-    return render(request,"index.html")
-def login_administrador(request):
-        loginAdmin = Login_admin(request.POST)
-        if loginAdmin.is_valid():
-            data = loginAdmin.cleaned_data
-            usuario = data.get("usuario")
-            contraseña = data.get("pass")
-            accesso = authenticate(username=usuario,password=contraseña)
-            if accesso is not None:
-                login(request,accesso)
-                return HttpResponse("Bienvenido")
-            else:
-                return HttpResponse("Usuario / Contraseña incorrectos")
-        else:
-            loginAdmin = Login_admin()
-
-        return render(request,"registration/login_administrador.html", {'loginAdmin': loginAdmin})
     
